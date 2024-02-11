c126992b8a7c282aefba3195f52039abbb5d1584
Tomasz Uszynski <tomaszuszynski3@gmail.com>
2024-02-11 23:32:33 +0000

    Advanced

--- python_accounts.py
+++ python_accounts.py
@@ -1,17 +1,24 @@
 # python_accounts.py
-# Program reads in a 10 character account number and outputs the account number
-# with only the last 4 digits showing (and the first 6 digits replaced with Xs).
+# Program reads in account number and outputs the account number
+# with only the last 4 digits showing (All digits except last 4 replaced with Xs).
 # author: Tomasz Uszynski
 
 
-accountNumber = input("Please enter a 10-digit account number.: ")  # Inputs 10 digits account number.
+accountNumber = input("Please enter an account number.: ")  # Inputs account number.
 
-# Because we know that length of account is 10 we know how many Xs we need to use to cover account number.
-# We use 6 Xs.
+accountNumberlength = len(accountNumber)                    # Checks length of account number.
 
+toHide = accountNumber[:-4]                                 # Declarates with part will be hide.
 
-print('XXXXXX' + accountNumber[-4:])                 # Displays result such as 6 Xs and last four digits of account number.
-                                                     # To find out last four digits we used negative indexing. 
-                                                     # In case of this program -1 refers to the last element,
-                                                     # -2 refers to the second-to-last element, and so on. Omnit end index
-                                                     # means the end of the sequence.
+lengthOftoHide = len(toHide)                                # Checks length of digits to hide.
+
+x = 'X'                                                     # Declarates sign used to cover group of digits.
+
+result = x * lengthOftoHide                                 # Counts how many Xs are needed.
+
+print(result+accountNumber[-4:])                            # Prints out expected result.
+                 
+# To find out last four digits we used negative indexing. 
+# In case of this program -1 refers to the last element,
+# -2 refers to the second-to-last element, and so on. Omnit end index
+# means the end of the sequence. Value result store information how many Xs are needed.
