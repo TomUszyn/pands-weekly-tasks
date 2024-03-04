6d0fc14a8f1ff9c58203c431ca4c9397da805be0
Tomasz Uszynski <tomaszuszynski3@gmail.com>
2024-03-04 20:36:57 +0000

    fstring formatting

--- bank.py
+++ bank.py
@@ -10,18 +10,20 @@
 
 amount1 = int(input("Enter amount1(in cent): "))    # Function input() take user input as a string, that's why we use
 amount2 = int(input("Enter amount2(in cent): "))    # we used function int() to convert string to integer to perform 
-                                                    # mathematical operations on it
+                                                    # mathematical operations on it.
 
 
-sum = amount1 + amount2                             # Adding two amounts and store them in a variable sum
+sum = amount1 + amount2                             # Adding two amounts and store them in a variable sum.
 
-txt = "The sum of these is \u20ac{conv:.2f}"        # Define text to print out with EUR currency with is \u20ac. 
-                                                    # conv should be in fixed point, two-decimal format
+txt = "The sum of these is \u20ac"                  # Define text to print out with EUR currency with is \u20ac. 
+                                                    # conv should be in fixed point, two-decimal format.
 
 
-print(txt.format(conv=sum/100))                     # The format() is used to insert values into a string. 
-                                                    # The value of conv is being replaced with the result of sum/100.
-                                                    # Conv is used to covert values from cents to EUR
+conv =sum/100                                       # The value of conv is being replaced with the result of sum/100.
+                                                    # Conv is used to covert values from cents to EUR.
+print(f'{txt}{conv: .2f}')                          # The f-string formatting is used to insert values into a string. 
+                                                    
+                                                    
                                                           
 # Useful links to help understand basics:
 # https://www.w3schools.com/python/default.asp           
