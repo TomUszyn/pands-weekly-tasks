a00e24b581cc5b47444f026cd20810fa92aa86a5
Tomasz Uszynski <tomaszuszynski3@gmail.com>
2024-03-06 00:16:21 +0000

    added research and references

--- collatz.py
+++ collatz.py
@@ -5,10 +5,20 @@
 # Have the program end if the current value is one.
 # author: Tomasz Uszynski
 
+# To write this program we to understand Collatz conjecture. Good source of information is Wikipedia.
+# We will need to understand the concept of loops and conditional statements.
+# We will use while loop to repeat the process until the current value is one.
+# We will use if statement to check if the current value is even or odd.
+# We will use the modulo operator (%) to check if the current value is even or odd.
+# We will use the floor division operator (//) to divide the current value by two.
+# We will use the multiplication operator (*) to multiply the current value by three.  
+# We will use the addition operator (+) to add one to the current value.   
+# We will use the end parameter of the print() function to print the values in the same line.
+
 # First of all user puts the positive integer.
 positiveInteger = int(input("Please enter an posivite integer: "))
 
-# Loop while checks if this is positive integer and f the entered integer is negative or zero,
+# Loop while checks if this is positive integer and if the entered integer is negative or zero,
 # it keeps asking for a positive integer.
 
 while positiveInteger <= 0:
@@ -27,4 +37,8 @@ while positiveInteger != 1:
         positiveInteger = positiveInteger // 2
     else:
         positiveInteger = positiveInteger*3 + 1
-print(positiveInteger, end = " ")                         
+print(positiveInteger, end = " ")
+
+# Referal links to help understand basics:
+# https://en.wikipedia.org/wiki/Collatz_conjecture
+# https://www.purdue.edu/hla/sites/varalalab/wp-content/uploads/sites/20/2018/04/Lecture_11.pdf       
